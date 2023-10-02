1cf2adefac5e59e064b575f2dc9631706a96778c
andy-spch <anderson.donaruma@gmail.com>
2023-10-02 16:46:00 -0300

    OrderStatus Enum

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -3,6 +3,7 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -30,11 +31,14 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+	private Integer orderStatus;
+
     public Order(){}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -54,7 +58,17 @@ public class Order implements Serializable {
         this.moment = moment;
     }
 
-    public User getClient() {
+    public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();			
+		}
+	}
+
+	public User getClient() {
         return client;
     }
 
